9d8a7a6777e6a1ffad5e7c5bfe42b5fea3859fd2
aychid <aydinnajlhossaini@hotmail.com>
2022-01-21 17:17:33 +0100

    updated grid

--- script.js
+++ script.js
@@ -5,27 +5,22 @@
     // Style the div's with css 
     // Individual squares are empty
 
-
-let squareNumber = 20;
-
-const smallBtn = document.querySelector("#small-grid");
-smallBtn.addEventListener("click", () => {
-    squareNumber = 20;
-})
-
 const divContainer = document.querySelector(".grid-container");
 
-for (let i = 0; i < squareNumber; i++){ // Outer loop initializes column 0, 1, 2, etc.. 
-    const divCol = document.createElement("div");
-    divCol.classList.add("grid-col");
-    divContainer.appendChild(divCol);
+function createGrid(squareNumber = 20) {
+    for (let i = 0; i < squareNumber; i++){ // Outer loop initializes column 0, 1, 2, etc.. 
+        const divCol = document.createElement("div");
+        divCol.classList.add("grid-col");
+        divContainer.appendChild(divCol);
 
-    for( let j = 0; j < squareNumber; j++){ // Inner loop initializes the items in the column
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("grid-item");
-        divCol.appendChild(newDiv);
-    }
+        for(let j = 0; j < squareNumber; j++){ // Inner loop initializes the items in the column
+            const newDiv = document.createElement("div");
+            newDiv.classList.add("grid-item");
+            divCol.appendChild(newDiv);
+        }
+    }    
 }
+createGrid();
 
 const items = document.querySelectorAll(".grid-item");
 items.forEach((item) => item.addEventListener("mouseover", () => {
